6d2975fed573fb6d0c5a4452837bec275a13dd64
vcjuliocesar <vcjuliocesar@outlook.com>
2023-08-30 14:32:01 -0600

    add token expire

--- utils/jwt_manager.py
+++ utils/jwt_manager.py
@@ -1,13 +1,22 @@
 import os
 from jwt import encode,decode
+from datetime import datetime,timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def create_token(data:dict) -> dict:
-    token:str = encode(payload=data,key=os.getenv('MY_SECRET_KEY'),algorithm="HS256")
+    payload = expire_token(data)
+    token:str = encode(payload,key=os.getenv('MY_SECRET_KEY'),algorithm="HS256")
     return token
 
 def validate_token(token:str) -> dict:
     data:dict = decode(token,key=os.getenv('MY_SECRET_KEY'),algorithms=["HS256"]) 
     return data
+
+def expire_token(data:dict):
+    to_encode = data.copy()
+    token_expires = timedelta(minutes=int(os.getenv('TOKEN_EXPIRE_MINUTES')))
+    expire = datetime.utcnow() + token_expires
+    to_encode.update({'exp':expire})
+    return to_encode
