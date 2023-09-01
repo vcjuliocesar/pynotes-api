f50aaeff308e66042b073d7cbedfa8c1f77cf950
vcjuliocesar <vcjuliocesar@outlook.com>
2023-09-01 15:28:46 -0600

    add postgresql requirements

--- config/database.py
+++ config/database.py
@@ -9,19 +9,14 @@ settings = Settings()
 
 if settings.ENVIROMENT == "production":
     conf = ProductionConfig
-    #database_url = f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}/{settings.DB_NAME}"
+    
     database_url = conf.DATABASE_URI
 
 else:
     conf = DevelopmentConfig
     
-    #sqlite_file_name = "../database.sqlite"
-
-    #base_dir = os.path.dirname(os.path.realpath(__file__))
-
-    #database_url = f"sqlite:///{os.path.join(base_dir,sqlite_file_name)}"
     database_url = conf.DATABASE_URI
-
+    
 engine = create_engine(database_url,echo=True)
 
 Session = sessionmaker(bind=engine)
