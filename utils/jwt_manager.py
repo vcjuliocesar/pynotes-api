ea2411b138c9210406dcb9b3f85fff523db78596
Armando Monreal <acalderon.root@gmail.com>
2023-09-02 20:46:25 -0600

    Apply responsability segregation principle in user route and jwt_manager

--- utils/jwt_manager.py
+++ utils/jwt_manager.py
@@ -1,21 +1,29 @@
-from jwt import encode,decode
-from datetime import datetime,timedelta
+from jwt import encode, decode
+from datetime import datetime, timedelta
 from utils.settings import Settings
 
 settings = Settings()
 
-def create_token(data:dict) -> dict:
-    payload = expire_token(data)
-    token:str = encode(payload,key=settings.MY_SECRET_KEY,algorithm="HS256")
+
+def create_token(data: dict) -> dict:
+    payload = add_expiration_date(data)
+
+    token: str = encode(payload, key=settings.MY_SECRET_KEY, algorithm="HS256")
     return token
 
-def validate_token(token:str) -> dict:
-    data:dict = decode(token,key=settings.MY_SECRET_KEY,algorithms=["HS256"]) 
+
+def validate_token(token: str) -> dict:
+    data: dict = decode(token, key=settings.MY_SECRET_KEY, algorithms=["HS256"])
+
     return data
 
-def expire_token(data:dict):
+
+def calculate_token_expiration():
+    return datetime.utcnow() + timedelta(minutes=settings.TOKEN_EXPIRE_MINUTES)
+
+
+def add_expiration_date(data: dict):
     to_encode = data.copy()
-    token_expires = timedelta(minutes=settings.TOKEN_EXPIRE_MINUTES)
-    expire = datetime.utcnow() + token_expires
-    to_encode.update({'exp':expire})
+    to_encode['exp'] = calculate_token_expiration()
+
     return to_encode
